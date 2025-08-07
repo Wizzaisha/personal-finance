5b1b1800146e57036567052a32fbd833879613f5
Wizzaisha <ever.ramirezmahecha@gmail.com>
2025-08-07 00:24:54 -0500

    a

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,31 +1,15 @@
-import Image, { getImageProps } from "next/image";
+import Image from "next/image";
 import { LoginIllustration } from "../ui/icons/images";
 import { LogoIconLarge } from "../ui/icons/svg";
-import getBackgroundImage from "../ui/utils/getBackgroundImage";
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  //   const {
-  //     props: { srcSet },
-  //   } = getImageProps({
-  //     alt: "authImage",
-  //     width: 560,
-  //     height: 920,
-  //     src: LoginIllustration,
-  //   });
-
-  //   console.log(srcSet);
-
-  //   const backgroundImage = getBackgroundImage(srcSet);
-
-  //   const style = { height: "100vh", width: "100vw", backgroundImage };
-
   return (
-    <section className="relative w-full min-h-dvh p-3 flex flex-row justify-start items-center">
-      <div className="absolute inset-3 z-[-1]">
+    <section className="w-full min-h-dvh p-3 flex flex-row justify-start items-center">
+      <div className="grid grid-cols-1 justify-between items-center">
         <Image
           src={LoginIllustration}
           width={560}
@@ -34,23 +18,24 @@ export default function AuthLayout({
           className="hidden xl:block rounded-xl"
           priority
         />
-      </div>
 
-      <div className="z-10 w-[560px] grid grid-cols-1 justify-between items-center">
+        <div className=" xl:hidden w-full bg-grey-900 absolute top-0 left-0 rounded-b-xl p-4 flex items-center justify-center">
+          <Image src={LogoIconLarge} width={122} height={22} alt="logoLarge" />
+        </div>
+
         <Image
           src={LogoIconLarge}
           width={122}
           height={22}
           alt="logoLarge"
-          className=""
-          priority
+          className="hidden xl:block absolute top-10 left-10"
         />
 
-        <div className="text-white">
+        <div className="text-white absolute bottom-10 left-10 w-[520px] hidden xl:block">
           <h3 className="text-preset1">
             Keep track of your money and save for your future
           </h3>
-          <p className="text-preset4-regular">
+          <p className="text-preset4-regular mt-5">
             Personal finance app puts you in control of your spending. Track
             transactions, set budgets, and add to savings pots easily.
           </p>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
+  },
 };
 
 export default nextConfig;
